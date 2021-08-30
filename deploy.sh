bed741e8d876ff7dc742d4f9e3d1caaf4d8b2e1a
Venkatraman K <venkatraman.ece@gmail.com>
2021-08-30 13:13:39 +0800

    added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t kuppusv/multi-client:latest -t kuppusv/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kuppusv/multi-server:latest -t kuppusv/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kuppusv/multi-worker:latest -t kuppusv/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push kuppusv/multi-client:latest
+docker push kuppusv/multi-server:latest
+docker push kuppusv/multi-worker:latest
+docker push kuppusv/multi-client:$SHA
+docker push kuppusv/multi-server:$SHA
+docker push kuppusv/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=kuppusv/multi-server:$SHA
+kubectl set image deployments/client-deployment server=kuppusv/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=kuppusv/multi-worker:$SHA
